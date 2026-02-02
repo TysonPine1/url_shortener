5b46abdce48b6c1136a63ae800bb402c05b8a07a
TysonPine <kaungthawmaung91@gmail.com>
2026-02-02 10:37:48 +0700

    URL short code uniqueness handled.

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,22 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-import fastapi.responses as RidrectResponse
+from fastapi.responses import RedirectResponse    
 from sqlalchemy.orm import Session
 
 from app.schemas.url import URLCreate, URLResponse
 from app.models.url import URL
 from app.services.shortener import generate_short_code
 from app.db.database import get_db
+from app.services.shortener import generate_unique_short_code
 
 router = APIRouter()
 
 
 @router.post("/shorten", response_model=URLResponse, status_code=status.HTTP_201_CREATED)
 def create_short_url(payload: URLCreate, db: Session = Depends(get_db)):
-    short_code = generate_short_code()
+    short_code = generate_unique_short_code(db)
 
     while db.query(URL).filter(URL.short_code == short_code).first():
-        short_code = generate_short_code()
-
+        short_code = generate_unique_short_code(db)
     url = URL(
         long_url=str(payload.long_url),
         short_code=short_code,
@@ -39,4 +39,4 @@ def redirect_url(short_code: str, db: Session = Depends(get_db)):
     url.access_count += 1
     db.commit()
 
-    return RidrectResponse(url=url.long_url)
+    return RedirectResponse(url=url.long_url)
--- app/services/shortener.py
+++ app/services/shortener.py
@@ -1,5 +1,15 @@
 import string
-import random 
+import random
+from sqlalchemy.orm import Session
+from app.models.url import URL
+ 
 def generate_short_code(length: int = 6) -> str:
     characters = string.ascii_letters + string.digits
     return "".join(random.choice(characters) for _ in range(length))
+
+def generate_unique_short_code(db: Session, length: int = 6) -> str:
+    while True:
+        short_code = generate_short_code(length)
+        exisits = db.query(URL).filter(URL.short_code == short_code).first()
+        if not exisits:
+            return short_code
