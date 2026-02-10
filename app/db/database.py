f1eb9cf60f65d38bf5132af41da8f5f0c7331a15
TysonPine <kaungthawmaung91@gmail.com>
2026-02-10 13:53:21 +0700

    "Removed data sensitive line of code"

--- app/db/database.py
+++ app/db/database.py
@@ -1,17 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base, Session
+from app.core.config import DATABASE_URL
 from fastapi import Depends
-import os
+import os   
 
-DB_USER = os.getenv("POSTGRES_USER", "postgres")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-DB_HOST = os.getenv("POSTGRES_HOST", "db")
-DB_PORT = os.getenv("POSTGRES_PORT", "5432")
-DB_NAME = os.getenv("POSTGRES_DB", "url_shortener")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
+
+Base = declarative_base()
+
 
 engine =  create_engine(DATABASE_URL)
 
